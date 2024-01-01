d6a6cc25d761e27e38e1993e914f3cfc28c5c3d5
Ruan Costa <ruancostadev@gmail.com>
2024-01-01 17:30:48 -0300

    Fix header text broken when animate

--- utils/textHoverAnimation.tsx
+++ utils/textHoverAnimation.tsx
@@ -4,7 +4,16 @@ import randomChar from "./randomChar";
 
 export default function textHoverAnimation(event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) {
 
+
     const targetElement = event.target as HTMLElement;
+
+    if(targetElement.classList.contains('hoverAnimating')){
+        return
+    }
+
+    if(!targetElement.classList.contains('hoverAnimating')){
+        targetElement.classList.add('hoverAnimating')
+    }
 
     if (!(targetElement instanceof HTMLElement)) {
         console.log('Não é um elemento html')
@@ -20,9 +29,15 @@ export default function textHoverAnimation(event: React.MouseEvent<HTMLAnchorEle
 
     }
 
-    for (let k = 0; k < originalText.length - 1; k++) {
+    for (let k = 0; k < originalText.length; k++) {
         setTimeout(() => {
+            
             targetElement.innerText = targetElement.innerText.substring(0, k) + originalText.charAt(k) + targetElement.innerText.substring(k + 1)
+
+            if(k == originalText.length - 1){
+                targetElement.classList.remove('hoverAnimating')
+            }
+
         }, k * 60)
     }
 
