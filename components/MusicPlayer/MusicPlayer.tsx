df2bf78a17a8b595e8f16b878444940ab29acb21
Ruan Costa Campos <ruancosta9797@gmail.com>
2024-08-15 20:59:18 -0300

    Add MusicPlayer component

--- components/MusicPlayer/MusicPlayer.tsx
+++ components/MusicPlayer/MusicPlayer.tsx
@@ -1,5 +1,14 @@
 "use client"
 
+import { useRef } from "react";
+
 export default function MusicPlayer(){
-    
+
+    const audioRef = useRef<HTMLAudioElement>(null);
+
+    return (
+        <div className="absolute left-0 top-0 ">
+            <audio ref={audioRef} src="/hover.mp3" />
+        </div>
+    )
 }
